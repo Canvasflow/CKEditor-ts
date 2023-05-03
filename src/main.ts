af9962df217e82dcc09026040225ba11616b58a9
Jose Juan <josejuan2412@hotmail.com>
2023-05-03 11:52:00 -0500

    feat: setting up pluggins

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import Balloon from "@ckeditor/ckeditor5-build-balloon";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { Underline } from "@ckeditor/ckeditor5-basic-styles";
+// import BalloonEditor from "./lib/BalloonEditor";
+import { BalloonEditor } from "@ckeditor/ckeditor5-editor-balloon";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 
-const editorPlaceholder = document.querySelector("#editor") as HTMLElement;
+const editorElement = document.querySelector("#editor") as HTMLElement;
 
-Balloon.create(editorPlaceholder).catch((error) => {
-  console.error(error);
-});
+BalloonEditor.create(editorElement, {
+  plugins: [Bold],
+  toolbar: {
+    items: ["bold"],
+  },
+})
+  .then((editor) => {
+    // window.editor = editor;
+  })
+  .catch(console.error);
