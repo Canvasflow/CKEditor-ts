27ddebc95fdbe0bbe61bd56c838bf78949f1c33e
Jose J. Cruz <jjzcru@gmail.com>
2023-05-29 12:49:06 -0500

    Remove plugins

--- src/plugins/colors/Colors.ts
+++
@@ -1,9 +0,0 @@
-import { ColorPickerUI } from "./ColorsUI";
-import { ColorEditing } from "./ColorsEditing";
-import { Plugin } from "@ckeditor/ckeditor5-core";
-
-export class Colors extends Plugin {
-  static get requires() {
-    return [ColorPickerUI, ColorEditing];
-  }
-}
