a5a118138fabc5c9c7d45290feacd7122599604d
Jose J. Cruz <jjzcru@gmail.com>
2023-06-01 14:50:01 -0500

    Add minify config

---
+++ vite.minify.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
+
+export default defineConfig({
+  build: {
+    lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'canvasflow-editor',
+        fileName: 'canvasflow-editor',
+    },
+},
+  plugins: [
+    dts(),
+    ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") })
+  ],
+});
