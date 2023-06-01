2f5dbb49e49b59e75e974e0f4f570349c29f862d
Jose Juan <josejuan2412@hotmail.com>
2023-06-01 17:29:31 -0500

    feat: :bug: input not showing up in image URL

--- src/TextEditor.ts
+++ src/TextEditor.ts
@@ -2,12 +2,12 @@ import BaseEditor, { TextEditorConfig } from "./BaseEditor";
 import { Essentials } from "@ckeditor/ckeditor5-essentials";
 
 import {
-    Bold,
-    Italic,
-    Underline,
-    Strikethrough,
-    Subscript,
-    Superscript,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  Subscript,
+  Superscript,
 } from "@ckeditor/ckeditor5-basic-styles";
 
 import { List } from "@ckeditor/ckeditor5-list";
@@ -16,17 +16,17 @@ import { Font } from "@ckeditor/ckeditor5-font";
 import { Alignment } from "@ckeditor/ckeditor5-alignment";
 import { Link } from "@ckeditor/ckeditor5-link";
 import {
-    Image,
-    ImageInsert,
-    AutoImage,
-    ImageResizeEditing,
-    ImageResizeHandles,
+  Image,
+  ImageInsert,
+  AutoImage,
+  ImageResizeEditing,
+  ImageResizeHandles,
 } from "@ckeditor/ckeditor5-image";
 import { Base64UploadAdapter } from "@ckeditor/ckeditor5-upload";
 
 import {
-    SpecialCharacters,
-    SpecialCharactersEssentials,
+  SpecialCharacters,
+  SpecialCharactersEssentials,
 } from "@ckeditor/ckeditor5-special-characters";
 
 import { defaultColors, customColorsSet } from "./plugins/Colors/ColorValues";
@@ -36,84 +36,86 @@ import { Colors } from "./plugins/Colors/Colors";
 import { PageLink } from "./plugins/PageLink/PageLink";
 import { SpecialCharactersEmoji } from "./plugins/SpecialCharactersEmoji/SpecialCharactersEmoji";
 
-
 export class TextEditor extends BaseEditor {
-    constructor(sourceElementOrData: HTMLElement | string, config?: TextEditorConfig) {
-        console.log(`Custom canvaflow editor loaded`)
-        if (config) {
-            config.plugins = PLUGINS;
-            config.toolbar = TOOLBAR
-            config.alignment = {
-                options: ["left", "right", "center", "justify"],
-            }
-        }
-
-        localStorage.setItem("defaultColors", JSON.stringify(defaultColors));
-        localStorage.setItem(
-            "customColorsSet",
-            JSON.stringify(JSON.stringify(Array.from(customColorsSet)))
-        );
-
-        super(sourceElementOrData, config);
+  constructor(
+    sourceElementOrData: HTMLElement | string,
+    config?: TextEditorConfig,
+  ) {
+    console.log(`Custom canvaflow editor loaded`);
+    if (config) {
+      config.plugins = PLUGINS;
+      config.toolbar = TOOLBAR;
+      config.alignment = {
+        options: ["left", "right", "center", "justify"],
+      };
     }
+
+    localStorage.setItem("defaultColors", JSON.stringify(defaultColors));
+    localStorage.setItem(
+      "customColorsSet",
+      JSON.stringify(JSON.stringify(Array.from(customColorsSet))),
+    );
+
+    super(sourceElementOrData, config);
+  }
 }
 
 const PLUGINS = [
-    Essentials,
-    Bold,
-    Italic,
-    List,
-    Paragraph,
-    Underline,
-    Strikethrough,
-    Subscript,
-    Superscript,
-    Font,
-    Alignment,
-    Link,
-    Timestamp,
-    SpecialCharacters,
-    SpecialCharactersEssentials,
-    SpecialCharactersEmoji as any,
-    Base64UploadAdapter,
-    Image,
-    ImageInsert,
-    AutoImage,
-    ImageResizeEditing,
-    ImageResizeHandles,
-    Colors,
-    DarkMode,
-    Timestamp,
-    PageLink,
-]
+  Essentials,
+  Bold,
+  Italic,
+  List,
+  Paragraph,
+  Underline,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Font,
+  Alignment,
+  Link,
+  Timestamp,
+  SpecialCharacters,
+  SpecialCharactersEssentials,
+  SpecialCharactersEmoji as any,
+  Base64UploadAdapter,
+  Image,
+  ImageInsert,
+  AutoImage,
+  ImageResizeEditing,
+  ImageResizeHandles,
+  Colors,
+  DarkMode,
+  Timestamp,
+  PageLink,
+];
 
 const TOOLBAR = [
-    "link",
-    "fontFamily",
-    "fontSize",
-    "colorPicker",
-    "fontBackgroundColor",
-    "|",
-    "bold",
-    "italic",
-    "underline",
-    "bulletedList",
-    "numberedList",
-    "strikethrough",
-    "subscript",
-    "superscript",
-    "|",
-    "alignment",
-    "|",
-    "insertImage",
-    "specialCharacters",
-    "dark-mode",
-    "pageLink",
-    // "timestamp",
+  "link",
+  "fontFamily",
+  "fontSize",
+  "colorPicker",
+  "fontBackgroundColor",
+  "|",
+  "bold",
+  "italic",
+  "underline",
+  "bulletedList",
+  "numberedList",
+  "strikethrough",
+  "subscript",
+  "superscript",
+  "|",
+  "alignment",
+  "|",
+  "insertImage",
+  "specialCharacters",
+  "dark-mode",
+  "pageLink",
+  // "timestamp",
 
-    // {
-    //   label: "More basic styles",
-    //   icon: "plus",
-    //   items: ["strikethrough", "superscript", "subscript"],
-    // },
-]
+  // {
+  //   label: "More basic styles",
+  //   icon: "plus",
+  //   items: ["strikethrough", "superscript", "subscript"],
+  // },
+];
