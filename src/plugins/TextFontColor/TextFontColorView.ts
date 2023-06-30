7cfa83f79ec79a3ff328e8d974022e965a344e64
Jose Juan <josejuan2412@hotmail.com>
2023-06-30 13:33:50 -0500

    feat: :fire: removed unused functions

--- src/plugins/TextFontColor/TextFontColorView.ts
+++ src/plugins/TextFontColor/TextFontColorView.ts
@@ -17,15 +17,14 @@ import { CLEAR_FONT_COLOR_COMMAND } from "./TextFontColorCommands";
 export class TextFontColorView extends View {
   private items: ViewCollection;
   private focusTracker: FocusTracker;
-  colors?: Colors;
-  private customColors?: ColorGridView;
   private editor: CanvasflowEditor;
-
   private removeColorButton?: ButtonView;
   private selectColorButton?: ButtonView;
+  private colorInput?: InputView;
+
+  colors?: Colors;
   defaultColorsGridView?: ColorsGridView;
   customColorsGridView?: ColorsGridView;
-  private colorInput?: InputView;
 
   constructor(locale: Locale, editor: CanvasflowEditor) {
     super(locale);
@@ -33,8 +32,6 @@ export class TextFontColorView extends View {
     this.focusTracker = new FocusTracker();
     this.items = this.createCollection();
 
-    // Add view to the component
-    // this.items.add(this.removeColorView)
     if (!editor.colors) {
       return;
     }
@@ -60,8 +57,6 @@ export class TextFontColorView extends View {
       },
       children: this.items,
     });
-
-    // this.setItems(this.colors);
   }
 
   private getRemoveColorView(): ButtonView {
@@ -107,53 +102,6 @@ export class TextFontColorView extends View {
     return colorInputView;
   }
 
-  private setItems(colors: Colors) {
-    this.items.add(this.createClearColorLabel());
-    this.items.add(this.createLabel("Default Colors"));
-    this.setDefaultColors(colors);
-    this.setCustomColors(colors);
-    this.addPickerButton();
-    this.items.add(this.createColorInput());
-    this.setTemplate({
-      tag: "form",
-      attributes: {
-        class: ["ck", "ck-colors"],
-      },
-      children: this.items,
-    });
-  }
-
-  private createClearColorLabel() {
-    let clearButton = this.createButton("Remove color", remove, "");
-    clearButton.type = "button";
-    clearButton.class = "clear-color-button";
-    clearButton.on("execute", () => {
-      this.editor.execute(CLEAR_FONT_COLOR_COMMAND);
-    });
-    return clearButton;
-  }
-
-  private setDefaultColors(colors: Colors) {
-    const defaultColorList = colors.defaultColor;
-    if (defaultColorList.length > 0) {
-      this.items.add(this.createColorsGrid(defaultColorList));
-    }
-  }
-
-  private setCustomColors(colors: Colors) {
-    this.items.add(this.createLabel("Custom colors"));
-    const colorList = colors.customColor;
-    this.customColors = this.createColorsGrid(colorList);
-    this.items.add(this.customColors);
-  }
-
-  private addPickerButton() {
-    let pickerButton = this.createButton("Select color", picker, "");
-    pickerButton.type = "submit";
-    pickerButton.class = "submit-color-button";
-    this.items.add(pickerButton);
-  }
-
   private createButton(label: any, icon: any, className: any) {
     const button = new ButtonView();
     button.set({
@@ -167,37 +115,6 @@ export class TextFontColorView extends View {
     return button;
   }
 
-  private createColorInput() {
-    const colorInput = new InputView(this.locale);
-    colorInput.id = "color-picker";
-    return colorInput;
-  }
-
-  private createColorsGrid(colors: any) {
-    const colorGridView = new ColorGridView(this.locale, {
-      colorDefinitions: colors.map((item: any) => {
-        item.label = item.color;
-        item.options = { hasBorder: true };
-        return item;
-      }),
-      columns: 4,
-    });
-
-    colorGridView.delegate("execute").to(this);
-    return colorGridView;
-  }
-
-  private createLabel(text: any) {
-    const labelView = new LabelView(this.locale);
-    labelView.text = text;
-    labelView.extendTemplate({
-      attributes: {
-        class: ["ck", "ck-color-grid__label"],
-      },
-    });
-    return labelView;
-  }
-
   render() {
     super.render();
     submitHandler({
@@ -208,19 +125,6 @@ export class TextFontColorView extends View {
   destroy() {
     super.destroy();
     this.focusTracker.destroy();
-  }
-
-  /**
-   * Insert new color into list
-   *
-   */
-  addCustomColor(color: string, label: string) {
-    const newColor = new ColorTileView(this.locale);
-    newColor.label = label;
-    newColor.color = color;
-    newColor.delegate("execute").to(this);
-    //this.customColors?.items.add(newColor);
-    this.customColorsGridView!.gridView.items.add(newColor);
   }
 
   /**
