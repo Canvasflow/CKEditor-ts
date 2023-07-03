d919687ce047200a6b059ba23ff9e9481e38d8d0
Jose Juan <josejuan2412@hotmail.com>
2023-07-03 11:52:51 -0500

    feat: :sparkles: bold view selection

--- src/plugins/FontStyles/BoldView.ts
+++ src/plugins/FontStyles/BoldView.ts
@@ -1,22 +1,52 @@
+import { ButtonView } from "@ckeditor/ckeditor5-ui";
 
-import {
-    ButtonView
-} from '@ckeditor/ckeditor5-ui';
-
-import icon from './../../assets/icons/bold.svg?raw'
-
-import { FontStylesViewer } from './FontStylesViewer';
+import icon from "./../../assets/icons/bold.svg?raw";
+import { FontStylesViewer } from "./FontStylesViewer";
+import CanvasflowEditor from "../../BaseEditor";
 
 export class BoldView extends ButtonView {
-    static viewName = 'boldCF'
-    constructor(viewer: FontStylesViewer) {
-        super(viewer.editor.locale)
-        this.label = 'Bold';
-        this.icon = icon;
-        this.tooltip = true;
-        this.withText = false;
-        this.listenTo(this, 'execute', () => {
-            viewer.onClickBold();
-        });
+  private editor: CanvasflowEditor;
+  static viewName = "boldCF";
+  constructor(viewer: FontStylesViewer) {
+    super(viewer.editor.locale);
+    const { editor } = viewer;
+    this.label = "Bold";
+    this.icon = icon;
+    this.tooltip = true;
+    this.withText = false;
+    this.class = "";
+    this.editor = editor;
+    this.listenTo(this, "execute", () => {
+      viewer.onClickBold();
+      this.focus();
+    });
+
+    this.init();
+  }
+
+  private init() {
+    const { model } = this.editor;
+    const { document } = model;
+    document.selection.on("change:range", this.onSelectionChange);
+  }
+
+  private onSelectionChange = () => {
+    const { selection } = this.editor.model.document;
+    if (!selection) {
+      return;
     }
+    const range = selection.getFirstRange();
+    if (!range) {
+      return;
+    }
+
+    for (const item of range.getItems()) {
+      if (item.hasAttribute("bold")) {
+        console.log("bold found");
+        this.class = "ck-on";
+        return;
+      }
+      this.class = "";
+    }
+  };
 }
