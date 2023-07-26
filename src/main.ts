368309be92567c8dcf986f7be6e1dfcd3f6fd0e9
Jose Juan <josejuan2412@hotmail.com>
2023-07-26 15:46:23 -0500

    docs: :memo: remove declared editors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { TextEditorConfig, PageAnchorSource } from "./BaseEditor";
+import { TextEditorConfig } from "./BaseEditor";
 import { TextEditor } from "./TextEditor";
-import { createListeners } from "./listeners";
-import { CustomEditor } from "./CustomEditor";
+// import { createListeners } from "./listeners";
+// import { CustomEditor } from "./CustomEditor";
 declare global {
   interface Window {
     CanvasflowTextEditor: any;
@@ -17,101 +17,101 @@ if (typeof window !== "undefined") {
   };
 }
 
-const customColor = [
-  { color: "yellow", label: "bright yellow" },
-  { color: "grey", label: "dark grey" },
-];
+// const customColor = [
+//   { color: "yellow", label: "bright yellow" },
+//   { color: "grey", label: "dark grey" },
+// ];
 
-const customBackgroundColor = [{ color: "orange", label: "orange" }];
+// const customBackgroundColor = [{ color: "orange", label: "orange" }];
 
-const config = {
-  pageLinkSources: [
-    {
-      id: "1111",
-      title: "Example",
-    },
-    {
-      id: "1112",
-      title: "Example 2",
-    },
-  ],
-  fontFamily: {
-    options: ["Georgia", "Ubuntu, Arial, sans-serif", "Times New Roman"],
-  },
-  fontSize: {
-    options: Array.from({ length: 70 }, (_, i) => i + 8),
-  },
+// const config = {
+//   pageLinkSources: [
+//     {
+//       id: "1111",
+//       title: "Example",
+//     },
+//     {
+//       id: "1112",
+//       title: "Example 2",
+//     },
+//   ],
+//   fontFamily: {
+//     options: ["Georgia", "Ubuntu, Arial, sans-serif", "Times New Roman"],
+//   },
+//   fontSize: {
+//     options: Array.from({ length: 70 }, (_, i) => i + 8),
+//   },
 
-  fetchAnchors,
-  colors: {
-    defaultColor: [
-      { color: "red", label: "red" },
-      { color: "blue", label: "blue" },
-      { color: "orange", label: "orange" },
-      { color: "teal", label: "teal" },
-      { color: "magenta", label: "magenta" },
-      { color: "grey", label: "grey" },
-      { color: "green", label: "green" },
-      { color: "#c3c3c3", label: "silver" },
-    ],
-    customColor,
-  },
-  fontBackground: {
-    defaultColor: [{ color: "red", label: "red" }],
-    customColor: customBackgroundColor,
-  },
-};
+//   fetchAnchors,
+//   colors: {
+//     defaultColor: [
+//       { color: "red", label: "red" },
+//       { color: "blue", label: "blue" },
+//       { color: "orange", label: "orange" },
+//       { color: "teal", label: "teal" },
+//       { color: "magenta", label: "magenta" },
+//       { color: "grey", label: "grey" },
+//       { color: "green", label: "green" },
+//       { color: "#c3c3c3", label: "silver" },
+//     ],
+//     customColor,
+//   },
+//   fontBackground: {
+//     defaultColor: [{ color: "red", label: "red" }],
+//     customColor: customBackgroundColor,
+//   },
+// };
 
-TextEditor.create(document.querySelector("#editor") as HTMLElement, config)
-  .then((editor) => {
-    createListeners(editor);
-    editor.addEventListener("textColor:addCustomColor", (evt: any) => {
-      const { color } = evt;
-      customColor.push({ color, label: "" });
-    });
+// TextEditor.create(document.querySelector("#editor") as HTMLElement, config)
+//   .then((editor) => {
+//     createListeners(editor);
+//     editor.addEventListener("textColor:addCustomColor", (evt: any) => {
+//       const { color } = evt;
+//       customColor.push({ color, label: "" });
+//     });
 
-    editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
-      const { color } = evt;
-      customBackgroundColor.push({ color, label: "" });
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+//     editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
+//       const { color } = evt;
+//       customBackgroundColor.push({ color, label: "" });
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-CustomEditor.build(document.querySelector("#editor2") as HTMLElement, config, [
-  "Bold",
-  "Separator",
-  "FontFamily",
-  "DarkMode",
-  "FontColor",
-  "HighlightColor",
-  { label: "Font Styles", icon: "fontStyles", items: ["Bold", "Italic"] },
-])
-  .then((editor) => {
-    createListeners(editor);
-    editor.addEventListener("textColor:addCustomColor", (evt: any) => {
-      const { color } = evt;
-      customColor.push({ color, label: "" });
-    });
+// CustomEditor.build(document.querySelector("#editor2") as HTMLElement, config, [
+//   "Bold",
+//   "Separator",
+//   "FontFamily",
+//   "DarkMode",
+//   "FontColor",
+//   "HighlightColor",
+//   { label: "Font Styles", icon: "fontStyles", items: ["Bold", "Italic"] },
+// ])
+//   .then((editor) => {
+//     createListeners(editor);
+//     editor.addEventListener("textColor:addCustomColor", (evt: any) => {
+//       const { color } = evt;
+//       customColor.push({ color, label: "" });
+//     });
 
-    editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
-      const { color } = evt;
-      customBackgroundColor.push({ color, label: "" });
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+//     editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
+//       const { color } = evt;
+//       customBackgroundColor.push({ color, label: "" });
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-async function fetchAnchors(id: string): Promise<Array<PageAnchorSource>> {
-  if (id === "1111") {
-    return [];
-  }
-  return [
-    {
-      id: "112233",
-      title: "asdasdasdasd",
-    },
-  ];
-}
+// async function fetchAnchors(id: string): Promise<Array<PageAnchorSource>> {
+//   if (id === "1111") {
+//     return [];
+//   }
+//   return [
+//     {
+//       id: "112233",
+//       title: "asdasdasdasd",
+//     },
+//   ];
+// }
