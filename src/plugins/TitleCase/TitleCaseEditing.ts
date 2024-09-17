f51f92827948e41499dac633e2c48b1bb303ec8a
Jose Juan <josejuan2412@hotmail.com>
2024-09-17 11:57:41 -0500

    feat: fixed bold color import

--- src/plugins/TitleCase/TitleCaseEditing.ts
+++ src/plugins/TitleCase/TitleCaseEditing.ts
@@ -1,3 +1,4 @@
+import { uid } from "ckeditor5/src/utils";
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import CanvasflowEditor from "../../BaseEditor";
 import {
@@ -28,6 +29,56 @@ export class TitleCaseEditing extends Plugin {
       isFormatting: true,
       copyOnEnter: true,
     });
+
+    editor.conversion.for("upcast").elementToAttribute({
+      view: {
+        name: "strong",
+      },
+      model: {
+        key: "bold",
+        value: (viewItem: any) => {
+          const attributes = ToAttribute(viewItem, {
+            style: viewItem.getAttribute("style"),
+          });
+
+          return attributes;
+        },
+      },
+      converterPriority: "high",
+    });
+
+    editor.conversion.for("upcast").elementToAttribute({
+      view: {
+        name: "b",
+      },
+      model: {
+        key: "bold",
+        value: (viewItem: any) => {
+          const attributes = ToAttribute(viewItem, {
+            style: viewItem.getAttribute("style"),
+          });
+
+          return attributes;
+        },
+      },
+      converterPriority: "high",
+    });
+
+    editor.conversion.for("downcast").attributeToElement({
+      model: "bold",
+      view: (modelAttributeValue, { writer }) => {
+        if (modelAttributeValue && modelAttributeValue.style) {
+          const attributes = { style: modelAttributeValue.style };
+
+          return writer.createAttributeElement("strong", attributes);
+        }
+
+        if (!modelAttributeValue) {
+          return;
+        }
+      },
+      converterPriority: "high",
+    });
   }
 }
 
@@ -40,4 +91,17 @@ function renderDowncastElement() {
       priority: 7,
     });
   };
+}
+
+function AddPluginAttributes(baseGlossaryData: any, data: any) {
+  return Object.assign({ uid: uid() }, baseGlossaryData, data || {});
+}
+
+function ToAttribute(viewElementOrGlossary: any, data: any) {
+  const textNode = viewElementOrGlossary.getChild(0);
+  if (!textNode) {
+    return;
+  }
+
+  return AddPluginAttributes({}, data);
 }
