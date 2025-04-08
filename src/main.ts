07a87d0022809670c65883843b734f9b80cdef54
Jose Juan <josejuan2412@hotmail.com>
2025-04-08 09:41:40 -0500

    feat: fix update title value

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { TextEditorConfig } from "./BaseEditor";
+import { PageAnchorSource, TextEditorConfig } from "./BaseEditor";
 import { TextEditor } from "./TextEditor";
 import { CustomEditor } from "./CustomEditor";
 import { version } from "./version";
-//import { createListeners } from "./listeners";
+import { createListeners } from "./listeners";
 
 declare global {
   interface Window {
@@ -34,112 +34,112 @@ if (typeof window !== "undefined") {
   window.CanvasflowCustomTextEditor.version = version;
 }
 
-// const customColor = [
-//   { color: "yellow", label: "bright yellow" },
-//   { color: "grey", label: "dark grey" },
-// ];
+const customColor = [
+  { color: "yellow", label: "bright yellow" },
+  { color: "grey", label: "dark grey" },
+];
 
-// const customBackgroundColor = [{ color: "grey", label: "dark grey" }];
+const customBackgroundColor = [{ color: "grey", label: "dark grey" }];
 
-// const config = {
-//   pageLinkSources: [
-//     {
-//       id: "1111",
-//       title: "Example",
-//     },
-//     {
-//       id: "1112",
-//       title: "Example 2",
-//     },
-//     {
-//       id: "1113",
-//       title: "Example 3",
-//     },
-//     {
-//       id: "1114",
-//       title: "Example 4",
-//     },
-//     {
-//       id: "1115",
-//       title: "Example 5",
-//     },
-//     {
-//       id: "1116",
-//       title: "Example 6",
-//     },
-//     {
-//       id: "1117",
-//       title: "Example 7",
-//     },
-//     {
-//       id: "1118",
-//       title: "Example 8",
-//     },
-//     {
-//       id: "1119",
-//       title: "Example 9",
-//     },
-//     {
-//       id: "1120",
-//       title: "Example 10",
-//     },
-//   ],
-//   fontFamily: {
-//     options: ["Georgia", "Ubuntu, Arial, sans-serif", "Times New Roman"],
-//   },
-//   fontSize: {
-//     options: Array.from({ length: 70 }, (_, i) => i + 8),
-//   },
+const config = {
+  pageLinkSources: [
+    {
+      id: "1111",
+      title: "Example",
+    },
+    {
+      id: "1112",
+      title: "Example 2",
+    },
+    {
+      id: "1113",
+      title: "Example 3",
+    },
+    {
+      id: "1114",
+      title: "Example 4",
+    },
+    {
+      id: "1115",
+      title: "Example 5",
+    },
+    {
+      id: "1116",
+      title: "Example 6",
+    },
+    {
+      id: "1117",
+      title: "Example 7",
+    },
+    {
+      id: "1118",
+      title: "Example 8",
+    },
+    {
+      id: "1119",
+      title: "Example 9",
+    },
+    {
+      id: "1120",
+      title: "Example 10",
+    },
+  ],
+  fontFamily: {
+    options: ["Georgia", "Ubuntu, Arial, sans-serif", "Times New Roman"],
+  },
+  fontSize: {
+    options: Array.from({ length: 70 }, (_, i) => i + 8),
+  },
 
-//   fetchAnchors,
-//   colors: {
-//     defaultColor: [
-//       { color: "red", label: "red" },
-//       { color: "blue", label: "blue" },
-//       { color: "orange", label: "orange" },
-//       { color: "teal", label: "teal" },
-//       { color: "magenta", label: "magenta" },
-//       { color: "grey", label: "grey" },
-//       { color: "green", label: "green" },
-//       { color: "#c3c3c3", label: "silver" },
-//     ],
-//     customColor,
-//   },
-//   fontBackground: {
-//     defaultColor: [{ color: "red", label: "red" }],
-//     customColor: customBackgroundColor,
-//   },
-//   caseChange: { titleCase: { excludeWords: [] } },
-// };
+  fetchAnchors,
+  colors: {
+    defaultColor: [
+      { color: "red", label: "red" },
+      { color: "blue", label: "blue" },
+      { color: "orange", label: "orange" },
+      { color: "teal", label: "teal" },
+      { color: "magenta", label: "magenta" },
+      { color: "grey", label: "grey" },
+      { color: "green", label: "green" },
+      { color: "#c3c3c3", label: "silver" },
+    ],
+    customColor,
+  },
+  fontBackground: {
+    defaultColor: [{ color: "red", label: "red" }],
+    customColor: customBackgroundColor,
+  },
+  caseChange: { titleCase: { excludeWords: [] } },
+};
 
-// TextEditor.create(document.querySelector("#editor") as HTMLElement, config)
-//   .then((editor) => {
-//     editor.setData(
-//       "<p><span style='color:rgb(245,122,5);font-family:PlayfairDisplay-MediumItalic;'><span style='color:rgb(0, 0, 0);'>Post-50, I’m more determined than ever to make every second count. I feel like there’s another book in me and I might like to dip m</span><sup title="title"><span style='color:rgb(0, 0, 0);'>y toe back into acting.</span></sup></span></p>",
-//     );
-//     // editor.setData("<b bold-color=true>world</b>");
-//     createListeners(editor);
-//     editor.addEventListener("textColor:addCustomColor", (evt: any) => {
-//       const { color } = evt;
-//       customColor.push({ color, label: "" });
-//     });
-//     editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
-//       const { color } = evt;
-//       customBackgroundColor.push({ color, label: "" });
-//     });
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+TextEditor.create(document.querySelector("#editor") as HTMLElement, config)
+  .then((editor) => {
+    editor.setData(
+      "<p>Other great works followed, astonishing the public and the critics.<i>The Slave Ship</i>(1840) showed the <u data-title='crew: members'>crew</u><strong><sup>18</sup></strong>of an English ship throwing dead and dying slaves overboard<strong><sup>19</sup></strong>. Turner’s painting, based on a true story, shocks with its “bloody<strong><sup>20</sup></strong>sky and flesh-filled<strong><sup>21</sup></strong>sea.” In<i>Rain, Steam, and Speed – The Great Western Railway</i>(1844), Turner’s work most synonymous with the Age of Steam, a speeding train crosses a bridge over the Thames, moving so fast that it is just a blur<strong><sup>22</sup></strong>.</p>",
+    );
+    // editor.setData("<b bold-color=true>world</b>");
+    createListeners(editor);
+    editor.addEventListener("textColor:addCustomColor", (evt: any) => {
+      const { color } = evt;
+      customColor.push({ color, label: "" });
+    });
+    editor.addEventListener("highlightColor:addCustomColor", (evt: any) => {
+      const { color } = evt;
+      customBackgroundColor.push({ color, label: "" });
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-// async function fetchAnchors(id: string): Promise<Array<PageAnchorSource>> {
-//   if (id === "1111") {
-//     return [];
-//   }
-//   return [
-//     {
-//       id: "112233",
-//       title: "asdasdasdasd",
-//     },
-//   ];
-// }
+async function fetchAnchors(id: string): Promise<Array<PageAnchorSource>> {
+  if (id === "1111") {
+    return [];
+  }
+  return [
+    {
+      id: "112233",
+      title: "asdasdasdasd",
+    },
+  ];
+}
--- src/plugins/TitleEditor/TitleEditorEditing.ts
+++ src/plugins/TitleEditor/TitleEditorEditing.ts
@@ -9,9 +9,6 @@ import {
 } from "./TitleEditorCommands";
 import { uid } from "ckeditor5/src/utils";
 
-const titles: Array<string> = [];
-const DATA_TITLE_EDITOR_ATTR = "data-title";
-
 export class TitleEditorEditing extends Plugin {
   static get pluginName() {
     return "TitleEditorEditing";
@@ -20,51 +17,8 @@ export class TitleEditorEditing extends Plugin {
   constructor(editor: CanvasflowEditor) {
     super(editor);
     editor.conversion.for("downcast").attributeToElement({
-      model: DATA_TITLE_EDITOR_ATTR,
-      view: () => {
-        // renderDowncastUElement(),
-      },
-    });
-
-    // editor.conversion.for("upcast").elementToAttribute({
-    //   view: {
-    //     name: "u",
-    //   },
-    //   model: {
-    //     key: TITLE_EDITOR_ATTR,
-    //     value: (viewItem: any) => {
-    //       const attributes = ToAttribute(viewItem, {
-    //         title: viewItem.getAttribute("title"),
-    //       });
-
-    //       if (attributes.title) {
-    //         return attributes.title;
-    //       }
-    //       return "";
-    //     },
-    //   },
-    //   converterPriority: "high",
-    // });
-
-    editor.conversion.for("upcast").elementToAttribute({
-      view: {
-        name: "span",
-      },
-      model: {
-        key: TITLE_EDITOR_ATTR,
-        value: (viewItem: any) => {
-          if (viewItem.getAttribute("title")) {
-            const attributes = ToAttribute(viewItem, {
-              title: viewItem.getAttribute("title"),
-            });
-
-            if (attributes.title) {
-              return attributes.title;
-            }
-          }
-        },
-      },
-      converterPriority: "high",
+      model: TITLE_EDITOR_ATTR,
+      view: renderDowncastElement(),
     });
 
     editor.conversion.for("upcast").elementToAttribute({
@@ -72,7 +26,7 @@ export class TitleEditorEditing extends Plugin {
         name: "u",
       },
       model: {
-        key: DATA_TITLE_EDITOR_ATTR,
+        key: TITLE_EDITOR_ATTR,
         value: (viewItem: any) => {
           const attributes = ToAttribute(viewItem, {
             "data-title": viewItem.getAttribute("data-title"),
@@ -81,33 +35,44 @@ export class TitleEditorEditing extends Plugin {
           if (attributes["data-title"]) {
             return attributes["data-title"];
           }
+          return "";
+        },
+      },
+      converterPriority: "high",
+    });
+
+    editor.conversion.for("upcast").elementToAttribute({
+      view: {
+        name: "span",
+      },
+      model: {
+        key: TITLE_EDITOR_ATTR,
+        value: (viewItem: any) => {
+          if (viewItem.getAttribute("data-title")) {
+            const attributes = ToAttribute(viewItem, {
+              "data-title": viewItem.getAttribute("data-title"),
+            });
+
+            if (attributes["data-title"]) {
+              return attributes["data-title"];
+            }
+          }
         },
       },
       converterPriority: "high",
     });
 
     editor.conversion.for("downcast").attributeToElement({
-      model: DATA_TITLE_EDITOR_ATTR,
-      view: (modelAttributeValue, { writer }) => {
-        if (modelAttributeValue && !titles.includes(modelAttributeValue)) {
-          console.log(
-            "inside DATA_TITLE_EDITOR_ATTR downcast",
-            modelAttributeValue,
-          );
-          titles.push(modelAttributeValue);
-          return writer.createAttributeElement("u", {
-            "data-title": modelAttributeValue,
-          });
-        }
-      },
-    });
-
-    editor.conversion.for("downcast").attributeToElement({
       model: TITLE_EDITOR_ATTR,
       view: (modelAttributeValue, { writer }) => {
-        if (modelAttributeValue) {
-          console.log("inside TITLE_EDITOR_ATTR downcast", modelAttributeValue);
-          titles.push(modelAttributeValue);
+        if (modelAttributeValue && modelAttributeValue.title) {
+          let downcastValues: any = {
+            title: modelAttributeValue["data-title"]
+              ? modelAttributeValue["data-title"]
+              : modelAttributeValue,
+          };
+          return writer.createAttributeElement("u", downcastValues);
+        } else {
           return writer.createAttributeElement("u", {
             "data-title": modelAttributeValue,
           });
@@ -118,39 +83,21 @@ export class TitleEditorEditing extends Plugin {
     editor.commands.add(TITLE_EDITOR_COMMAND, new TitleEditorCommand(editor));
     editor.commands.add(TITLE_EDITOR_CLEAR, new ClearTitleCommand(editor));
     editor.model.schema.extend("$text", { allowAttributes: TITLE_EDITOR_ATTR });
-    editor.model.schema.extend("$text", {
-      allowAttributes: DATA_TITLE_EDITOR_ATTR,
-    });
     editor.model.schema.setAttributeProperties(TITLE_EDITOR_ATTR, {
-      isFormatting: true,
-      copyOnEnter: true,
-    });
-    editor.model.schema.setAttributeProperties(DATA_TITLE_EDITOR_ATTR, {
       isFormatting: true,
       copyOnEnter: true,
     });
   }
 }
 
-// function renderDowncastSupElement() {
-//   return (modelAttributeValue: any, viewWriter: any) => {
-//     console.log("sup element called");
-//     const attributes = { title: modelAttributeValue };
-//     return viewWriter.writer.createAttributeElement("sup", attributes, {
-//       priority: 7,
-//     });
-//   };
-// }
-
-// function renderDowncastUElement() {
-//   return (modelAttributeValue: any, viewWriter: any) => {
-//     console.log("u element called");
-//     const attributes = { title: modelAttributeValue };
-//     return viewWriter.writer.createAttributeElement("u", attributes, {
-//       priority: 7,
-//     });
-//   };
-// }
+function renderDowncastElement() {
+  return (modelAttributeValue: any, viewWriter: any) => {
+    const attributes = { "data-title": modelAttributeValue };
+    return viewWriter.writer.createAttributeElement("u", attributes, {
+      priority: 7,
+    });
+  };
+}
 
 function ToAttribute(viewElementOrGlossary: any, data: any) {
   const textNode = viewElementOrGlossary.getChild(0);
